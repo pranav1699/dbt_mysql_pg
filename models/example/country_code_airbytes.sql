cc2deb89fa7daa55fbb9d60df134e88ad6d4a33d
pranav1699 <pranavsadagopan@gmail.com>
2022-03-21 14:29:57 +0530

    initial commit

---
+++ models/example/country_code_airbytes.sql
@@ -0,0 +1,10 @@
+{{config(materialized="table") }}
+with country_code_airbytes as(
+    select * from {{ source('airbyte_test', 'airbytesuser') }}
+),
+final as (
+    select * from country_code_airbytes where username ='pranav'
+)
+
+
+select * from final

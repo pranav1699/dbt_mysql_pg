aa55f60f43bca420c0b75872b02b47ec1abc631b
pranav1699 <pranavsadagopan@gmail.com>
2022-03-21 15:19:17 +0530

    7

--- models/example/country_code_airbytes.sql
+++ models/example/country_code_airbytes.sql
@@ -1,6 +1,6 @@
 {{config(materialized="table") }}
 with country_code_airbytes as(
-    select * from {{ source('airbyte_test', 'airbytesuser') }}
+    select * from {{ source('airbytes_test', 'airbytesuser') }}
 ),
 final as (
     select * from country_code_airbytes where username ='pranav'
